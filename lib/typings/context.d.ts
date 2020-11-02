df3bb6db8e67be7ad269332b2fd6c08196829d3c
feddot2517@gmail.com <8D2g1101>
2020-11-02 16:06:10 +0600

    add new types

--- lib/typings/context.d.ts
+++ lib/typings/context.d.ts
@@ -1,6 +1,19 @@
-export declare class Context {
+export declare class Context implements VkontakteContext {
     constructor(event: {type: any, object: any, }, bot: object);
-    message: any;
+    message: VkontakteContextMessage;
     client_info?: any;
     user: any;
+    reply(text: string, extra?: object): Promise<any>;
+}
+
+export interface VkontakteContext {
+    message: VkontakteContextMessage;
+    client_info?: any;
+    user: any;
+    reply(text: string, extra?: object): Promise<any>;
+}
+
+export interface VkontakteContextMessage {
+    ref?: string,
+    user_id: string,
 }

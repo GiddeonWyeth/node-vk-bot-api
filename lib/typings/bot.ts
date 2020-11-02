b7914a9fd21e4a32ee19fffa2a2c0c8e694593f4
feddot2517@gmail.com <8D2g1101>
2020-11-02 16:48:34 +0600

    update types

--- lib/typings/bot.ts
+++ lib/typings/bot.ts
@@ -1,7 +1,10 @@
+import {VkontakteContext} from "./context";
+
 export declare class VkBot {
     constructor(token: string);
+    use(ctx: VkontakteContext, next: Function):void;
     event(triggers: any, middlewares: Array<any>): void;
     on(middlewares: Array<any>): void;
-    startPooling(): Promise<any>;
+    startPooling(callback: Function): Promise<any>;
     sendMessage(chatId: string, text: string, extra?: object): Promise<any>;
 }

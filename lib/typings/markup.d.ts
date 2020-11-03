1ccdf8a6b166dd90371f29bc951d8ee691a46a8b
feddot2517@gmail.com <8D2g1101>
2020-11-03 18:45:43 +0600

    fix markup typings again

--- lib/typings/markup.d.ts
+++ lib/typings/markup.d.ts
@@ -5,10 +5,19 @@ export declare class Markup {
 
     inline(value?: boolean): string;
 
+    static keyboard(buttons: any, options?: { columns: any }): Markup;
+
     toJSON(): string
 
     static button(label: string, color?: string, payload?: { button: any }): VkMarkupButton
 
 }
 
-export declare type VkMarkupButton = { action: { type: string, payload: string, label: string }, color: string };
+export interface VkMarkupButton {
+    action: {
+        type: string,
+        payload: string,
+        label: string
+    },
+    color: string
+}
